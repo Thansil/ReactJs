e667fb59aea69daeef7094a3ae678002c1446119
Thansil <60086144+Thansil@users.noreply.github.com>
2021-04-21 09:20:37 +0530

    Add files via upload

---
+++ projectfront/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import ListStudentComponent from './ListStudentComponent';
+
+function App() {
+  return (
+    <ListStudentComponent />
+  );
+}
+
+export default App;
